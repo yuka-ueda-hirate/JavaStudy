4b524bf254a833a5b4d5dbc9dd65ef3b525f195d
yuka_ueda <yuka_ueda@hirate.com>
2020-04-09 10:45:56 +0900

    version0

---
+++ Sum.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class Sum {
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+        sum.execute();
+    }
+
+    void execute() {
+        outputSum(1,2);
+        outputSum(2,22);
+        outputSum(3,10);
+    }
+
+    //メソッド(処理のブロック)
+    //メソッド名：outputSum
+    void outputSum(int numberA,int numberB){
+        //System.out.println(numberA + numberB);
+        System.out.println(sum(numberA,numberB));
+    }
+
+    //int型の戻り値がある（足した結果を返す）
+    int sum(int numberA,int numberB){
+        return numberA + numberB;
+    }
+}
